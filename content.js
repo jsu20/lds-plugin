317283ff5ab2d21f24aa0852cd4769c2e15a29ba
Jocelin Su <jocelin.su@salesforce.com>
2020-06-16 12:24:03 -0700

    added d3 dag visualization

--- content.js
+++ content.js
@@ -1,4 +1,108 @@
 // get data from current active tab
-let source = document.body.innerHTML;
-// send putSource command 
-chrome.runtime.sendMessage({action: 'putSource', source: source});
+// $mds = (function getModuleDefRegistry() {
+//   const cs = $A.componentService;
+//   const moduleDefRegistry = Object.keys(cs).find(key => { return cs[key] !== null && !Array.isArray(cs[key]) && typeof cs[key] === 'object' && cs[key]['force/lds']; })
+//   return cs[moduleDefRegistry];
+// })();
+// $lds = (function getLdsInstance(mds) {
+//   const def = mds['force/lds'];
+//   const instance = Object.keys(def).find(key => { return def[key] !== null && !Array.isArray(def[key]) && typeof def[key] === 'object'; })
+//   return def[instance];
+// })($mds);
+// $ads = (function getRecordLibraryInstance(mds) {
+//   const def = mds['markup://force:recordLibrary'];
+//   const instance = Object.keys(def).find(key => { return def[key] !== null && !Array.isArray(def[key]) && typeof def[key] === 'object'; })
+//   return def[instance];
+// })($mds);
+// send putSource command
+let grafo = [
+  {
+    "id": "0",
+    "parentIds": ["8"]
+  },
+  {
+    "id": "1",
+    "parentIds": []
+  },
+  {
+    "id": "2",
+    "parentIds": []
+  },
+  {
+    "id": "3",
+    "parentIds": ["11"]
+  },
+  {
+    "id": "4",
+    "parentIds": ["12"]
+  },
+  {
+    "id": "5",
+    "parentIds": ["18"]
+  },
+  {
+    "id": "6",
+    "parentIds": ["9", "15", "17"]
+  },
+  {
+    "id": "7",
+    "parentIds": ["3", "17", "20", "21"]
+  },
+  {
+    "id": "8",
+    "parentIds": []
+  },
+  {
+    "id": "9",
+    "parentIds": ["4"]
+  },
+  {
+    "id": "10",
+    "parentIds": ["16", "21"]
+  },
+  {
+    "id": "11",
+    "parentIds": ["2"]
+  },
+  {
+    "id": "12",
+    "parentIds": ["21"]
+  },
+  {
+    "id": "13",
+    "parentIds": ["4", "12"]
+  },
+  {
+    "id": "14",
+    "parentIds": ["1", "8"]
+  },
+  {
+    "id": "15",
+    "parentIds": []
+  },
+  {
+    "id": "16",
+    "parentIds": ["0"]
+  },
+  {
+    "id": "17",
+    "parentIds": ["19"]
+  },
+  {
+    "id": "18",
+    "parentIds": ["9"]
+  },
+  {
+    "id": "19",
+    "parentIds": []
+  },
+  {
+    "id": "20",
+    "parentIds": ["13"]
+  },
+  {
+    "id": "21",
+    "parentIds": []
+  }
+];
+chrome.runtime.sendMessage({action: 'putSource', source: grafo});
--- newTab.js
+++ newTab.js
@@ -1,3 +1,107 @@
 chrome.runtime.sendMessage({action: 'getSource'}, function(response) {
-    document.body.innerHTML = response.source;
+  grafo = response.source;
+  alert(grafo);
+  const dag = d3.dagStratify()(grafo)
+
+  d3.sugiyama()(dag);
+
+  const links = dag.links()
+  const descendants = dag.descendants();
+
+  const width = 400;
+  const height = 400;
+
+  const steps = dag.size();
+  const interp = d3.interpolateRainbow;
+  const colorMap = {};
+  dag.each((node, i) => {
+    colorMap[node.id] = interp(i / steps);
+  });
+  const nodeRadius=20;
+  const svgSelection = d3.select('svg');
+
+  const line = d3.line()
+    .curve(d3.curveCatmullRom)
+    .x(d => d.x * width)
+    .y(d => d.y * height);
+
+  const g = svgSelection.append('g').attr('transform', `translate(${100},${100})`)
+  const defs = g.append('defs'); // For gradients
+  g.append('g')
+    .selectAll('path')
+    .data(dag.links())
+    .enter()
+    .append('path')
+    .attr('d', ({ data }) => line(data.points))
+    .attr('fill', 'none')
+    .attr('stroke-width', 3)
+    .attr('stroke', ({source, target}) => {
+      const gradId = `${source.id}-${target.id}`;
+      const grad = defs.append('linearGradient')
+        .attr('id', gradId)
+        .attr('gradientUnits', 'userSpaceOnUse')
+        .attr('x1', width*source.x)
+        .attr('x2', width*target.x)
+        .attr('y1', height*source.y)
+        .attr('y2', height*target.y);
+      grad.append('stop').attr('offset', '0%').attr('stop-color', colorMap[source.id]);
+      grad.append('stop').attr('offset', '100%').attr('stop-color', colorMap[target.id]);
+      return `url(#${gradId})`;
+    });
+
+  const nodes = g.append('g')
+    .selectAll('g')
+    .data(descendants)
+    .enter()
+    .append('g')
+    .attr('transform', ({
+      x,
+      y
+    }) => `translate(${x*width}, ${y*height})`);
+
+  nodes.append('circle')
+    .attr('r', 20)
+    .attr('fill', n => colorMap[n.id]);
+    //.attr('stroke', 'black');
+
+
+  // Generate arrows
+  const arrow = d3.symbol().type(d3.symbolTriangle).size(nodeRadius * nodeRadius / 5.0);
+  g.append('g')
+    .selectAll('path')
+    .data(dag.links())
+    .enter()
+    .append('path')
+    .attr('d', arrow)
+    .attr('transform', ({
+      source,
+      target,
+      data
+    }) => {
+      const [end, start] = data.points.reverse();
+      console.log([end, start]);
+      // This sets the arrows the node radius (20) + a little bit (3) away from the node center, on the last line segment of the edge. This means that edges that only span ine level will work perfectly, but if the edge bends, this will be a little off.
+      const dx = width * (start.x - end.x);
+      const dy = height * (start.y - end.y);
+      const scale = nodeRadius * 1.15 / Math.sqrt(dx * dx + dy * dy);
+      // This is the angle of the last line segment
+      const angle = Math.atan2(-dy, -dx) * 180 / Math.PI + 90;
+      console.log(angle, dx, dy);
+      return `translate(${width * end.x + dx * scale}, ${height * end.y + dy * scale}) rotate(${angle})`;
+    })
+    .attr('fill', ({target}) => colorMap[target.id])
+    .attr('stroke', 'white')
+    .attr('stroke-width', 1.5);
+
+  // Add text
+  nodes.append('text')
+    .text(d => d.id)
+    .attr('font-weight', 'bold')
+    .attr('font-family', 'sans-serif')
+    .attr('text-anchor', 'middle')
+    .attr('alignment-baseline', 'middle')
+    .attr('fill', 'white');
+
+
+
 });
